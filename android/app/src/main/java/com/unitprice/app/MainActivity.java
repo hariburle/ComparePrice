dbf82fab89fab153aa7d0daeba112b41e8dfa771
hariburle <hariburle@gmail.com>
2026-08-11 20:16:53 -0500

    feat: add Capacitor Android platform project

---
+++ android/app/src/main/java/com/unitprice/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.unitprice.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.unitprice.app',
+  appName: 'Unit Price Compare',
+  webDir: 'dist'
+};
+
+export default config;
